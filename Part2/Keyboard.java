da5b145d4e7de250f11b74159c05b8996eece35b
Lauren Ingraham <laureningy@gmail.com>
2023-04-22 10:02:59 -0400

    Homework 6 Upload

---
+++ Part2/Keyboard.java
@@ -0,0 +1,8 @@
+package Part2;
+
+public class Keyboard implements ComputerPart {
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+        computerPartVisitor.visit(this);
+    }
+}
